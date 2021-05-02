299f61d5355f8c20bb8fcd6501824be842794ac3
vicky <srivicky2000@gmail.com>
2021-05-02 16:37:16 +0530

    Wbmw

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const wbm = require('wbm');
+const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
-wbm.start().then(async() => {
-    const phones = ['+919842279641'];
-    const message = 'Verification code : 34GJ%3';
-    await wbm.send(phones, message);
-    await wbm.end();
-}).catch(err => console.log(err));
+app.get("/", () => {
+    wbm.start().then(async() => {
+        const phones = ['+919842279641'];
+        const message = 'Verification code : 34GJ%3';
+        await wbm.send(phones, message);
+        await wbm.end();
+    }).catch(err => console.log(err));
+});
+
+
+app.listen(port, () => {
+    console.log("start");
+});
